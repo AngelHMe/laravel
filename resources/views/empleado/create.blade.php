23d6021effef89419f0bf7b7f2b8680c18d0beb2
unknown <thereaiyhernandez@gmail.com>
2024-11-07 12:36:47 -0700

    first commit

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,14 @@
+
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="Nombre" class="form-label">Nombre</label>
+        <input 
+            type="text"
+            class="form-control"
+            name="Nombre"
+            id="Nombre"
+            aria-describedby="helpId"
+            placeholder="Nombre"
+        />
+
+    </div>
